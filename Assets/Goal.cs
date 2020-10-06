61cf7e3ed5752a292b5ba7fc9ff04249456accb4
Nucky <enoragon@gmail.com>
2020-10-05 20:52:21 -0500

    Add restart method

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Goal : MonoBehaviour
@@ -9,5 +10,9 @@ public class Goal : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         WinUI.SetActive(true);
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
